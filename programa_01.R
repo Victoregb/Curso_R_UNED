eba207fb4f5066cbd0cb667e418513a5d660a75e
Victoregb <Victoregb@users.noreply.github.com>
2021-04-15 11:15:16 +0200

    Add files via upload

---
+++ programa_01.R
@@ -0,0 +1,13 @@
+# programa_1.R
+# autor:victoregb; 15/04/2021
+
+#Paso 3: Crea un vector v, de longitud 7, intervalo [0,10]
+v <- c(0,3,5, 7,9,1,10)
+
+#El paso 4 y 5 lo resumimos en una sola linea.
+print(mean(v))
+
+#Guardamos el archivo con el nombre 'programa_1.R'
+#Comprobamos que esta con ¿¿getwd()?? La ayuda nos recomienda list.files y
+# corroboramos que tenemos el programa. Esta celda la he copiado en un
+# script de R y guardado.
---
+++ programa_02.R
@@ -0,0 +1,4 @@
+data <- load("~/Curso_R/datos.Rdata")
+
+media <- mean(v)
+cat("La media es: ", media)
